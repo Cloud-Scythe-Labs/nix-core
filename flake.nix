4d50a83aac006027d77485cb54a1c16d48475453
eureka-cpu <github.eureka@gmail.com>
2024-07-08 21:38:41 -0700

    fmt using nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -12,24 +12,24 @@
   };
 
   outputs = { self, nixpkgs, fenix, ... }:
-  let
-    rustToolchainFromTOML = import ./toolchains/rust-toolchain.nix;
-  in
-  {
-    # Handles building project specific toolchains.
-    toolchains = {
-      # Assumes a `rust-toolchain.toml` is present in the project root
-      # and produces the toolchain from it for the given system.
-      # Useful for rust projects that declare a `rust-toolchain.toml`.
-      mkRustToolchainFromTOML = lib: pkgs: system: toml_path:
-        rustToolchainFromTOML {
-          inherit
-            lib
-            pkgs
-            fenix
-            system
-            toml_path;
-        };
+    let
+      rustToolchainFromTOML = import ./toolchains/rust-toolchain.nix;
+    in
+    {
+      # Handles building project specific toolchains.
+      toolchains = {
+        # Assumes a `rust-toolchain.toml` is present in the project root
+        # and produces the toolchain from it for the given system.
+        # Useful for rust projects that declare a `rust-toolchain.toml`.
+        mkRustToolchainFromTOML = lib: pkgs: system: toml_path:
+          rustToolchainFromTOML {
+            inherit
+              lib
+              pkgs
+              fenix
+              system
+              toml_path;
+          };
+      };
     };
-  };
 }
